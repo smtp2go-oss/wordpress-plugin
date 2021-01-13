7ce7ebfe3505c96d1e16afe20df10bdd575633b4
Kris Johansen <kris.johansen@gmail.com>
2021-01-14 10:01:29 +1300

    swap the wp version logic around

--- smtp2go-wordpress-plugin.php
+++ smtp2go-wordpress-plugin.php
@@ -100,11 +100,7 @@ if (!function_exists('wp_mail') && get_option('smtp2go_enabled')) {
         // (Re)create it, if it's gone missing
         //handle wordpress pre 5.5
 
-        if (is_file(ABSPATH . WPINC . '/class-phpmailer.php') && !($phpmailer instanceof PHPMailer)) {
-            require_once ABSPATH . WPINC . '/class-phpmailer.php';
-            require_once ABSPATH . WPINC . '/class-smtp.php';
-            $phpmailer = new PHPMailer(true);
-        } elseif (is_file(ABSPATH . WPINC . '/PHPMailer/PHPMailer.php') && !($phpmailer instanceof PHPMailer\PHPMailer\PHPMailer)) {
+        if (is_file(ABSPATH . WPINC . '/PHPMailer/PHPMailer.php') && !($phpmailer instanceof PHPMailer\PHPMailer\PHPMailer)) {
             //5.5 and up
             require_once ABSPATH . WPINC . '/PHPMailer/PHPMailer.php';
             require_once ABSPATH . WPINC . '/PHPMailer/SMTP.php';
@@ -114,6 +110,10 @@ if (!function_exists('wp_mail') && get_option('smtp2go_enabled')) {
             $phpmailer::$validator = static function ($email) {
                 return (bool) is_email($email);
             };
+        } elseif (is_file(ABSPATH . WPINC . '/class-phpmailer.php') && !($phpmailer instanceof PHPMailer)) {
+            require_once ABSPATH . WPINC . '/class-phpmailer.php';
+            require_once ABSPATH . WPINC . '/class-smtp.php';
+            $phpmailer = new PHPMailer(true);
         }
 
         //let other plugins modify the arguments as the native wp mail does
