f1a95f264ab74f4a8ffaeb9c8489ada6ede5f224
Kris Johansen <kris.johansen@gmail.com>
2021-04-19 15:26:31 +1200

    output result from api call or error message

--- admin/partials/smtp2go-wordpress-plugin-validation-display.php
+++ admin/partials/smtp2go-wordpress-plugin-validation-display.php
@@ -1,6 +1,17 @@
 <div class="wrap smtp2go">
-<h1>Sender Domain Validation for <?php echo get_site_url() ?></h1>
-<pre>
-<?php print_r($result) ?>
-</pre>
+<h1>Sender Domain Validation for <?php echo $this_host ?></h1>
+
+<?php if (!empty($result->domains)) : ?>
+
+<?php foreach ($result->domains as $domain) : ?>
+
+<pre><?php echo print_r($domain, 1); ?></pre>
+
+<?php endforeach; ?>
+
+<?php else : ?>
+
+<div class="error">Unable to retrieve domain validation information. <strong><?php echo $result->error ?? '' ?></strong></div>
+<?php endif ?>
+
 </div>
