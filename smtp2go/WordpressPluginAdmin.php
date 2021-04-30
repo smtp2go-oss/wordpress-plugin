39eabba43c20a0f4b2ad61642affd39d2e4f4416
Kris Johansen <kris.johansen@gmail.com>
2021-04-30 12:07:08 +1200

    determine the domain and tracker status

--- smtp2go/WordpressPluginAdmin.php
+++ smtp2go/WordpressPluginAdmin.php
@@ -398,16 +398,21 @@ class WordpressPluginAdmin
 
     public function renderValidationPage()
     {
-        $domain = new ApiDomain;
+        $domain  = new ApiDomain;
         $request = new ApiRequest(get_option('smtp2go_api_key'));
 
-        $sender  = new WordpressHttpRemotePostSender;
+        $sender = new WordpressHttpRemotePostSender;
         $sender->setTimeout(60);
-        $result = null;
+        $result    = null;
         $this_host = parse_url(get_site_url(), PHP_URL_HOST);
 
         $request->send($domain->verify($this_host), $sender);
-        $result = $sender->getLastResponse()->data ?? null;
+        $result                 = $sender->getLastResponse()->data ?? null;
+        $domain_info            = $result->domains[0]->domain ?? null;
+        $tracker_info           = $result->domains[0]->trackers[0] ?? null;
+        $domain_status_good     = !empty($domain_info) && $domain_info->dkim_verified && $domain_info->rpath_verified;
+        $tracker_status_good    = !empty($tracker_info) && $tracker_info->cname_verified;
+        $tracker_status_enabled = !empty($tracker_info) && $tracker_info->enabled;
 
         require_once plugin_dir_path(dirname(__FILE__)) . 'admin/partials/smtp2go-wordpress-plugin-validation-display.php';
     }
