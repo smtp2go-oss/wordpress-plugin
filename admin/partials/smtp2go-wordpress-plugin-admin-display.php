864a2a3755a19ab434b9454dae8745ed24f0472a
Mark Collister <mark@2050.nz>
2022-08-29 13:17:49 +1200

    Sender Domains is now called Verified Senders

--- admin/partials/smtp2go-wordpress-plugin-admin-display.php
+++ admin/partials/smtp2go-wordpress-plugin-admin-display.php
@@ -63,7 +63,7 @@ function SMTP2GO_tab_active($tab)
                 class="js-stats-tab-span spinner" style="float: none; display: none; margin: 0px 10px 2px ;"></span></a>
 
                 <a href="<?php echo admin_url(add_query_arg(array('page' => 'smtp2go-wordpress-plugin', 'tab' => 'validation'), 'admin.php')) ?>"
-            class="nav-tab <?php echo SMTP2GO_tab_active('validation') ?> js-validation-tab">Sender Domain<span
+            class="nav-tab <?php echo SMTP2GO_tab_active('validation') ?> js-validation-tab">Verified Senders<span
                 class="js-validation-tab-span spinner" style="float: none; display: none; margin: 0px 10px 2px ;"></span></a>
         <?php endif;?>
 
--- admin/partials/smtp2go-wordpress-plugin-validation-display.php
+++ admin/partials/smtp2go-wordpress-plugin-validation-display.php
@@ -44,5 +44,5 @@
 </div>
 <div class="smtp2go-validation-help-link">
     <a target="_blank" href="https://support.smtp2go.com/hc/en-gb/articles/115004408567-Sender-Domains">What is
-        Sender Domain Validation?</a>
+        Verified Senderss?</a>
 </div>
